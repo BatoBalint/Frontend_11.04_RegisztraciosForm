5a6ef7e09a0df942156a25dc388a4518a8796b4a
BatoBalint <balint.bato@gmail.com>
2021-11-04 21:14:03 +0100

    alakul a js, html elementek hozzaadva

---
+++ main.js
@@ -0,0 +1,10 @@
+var nevDiv, nevDb, nevInput, jelszoDiv, jelszoDb, jelszoInput, jelszoMegintInput;
+
+function init() {
+  nevDiv = document.getElementById('nameCharCountDiv');
+  nevDb = document.getElementById('nameCharCount');
+  jelszoDiv = document.getElementById('passCharCountDiv');
+  jelszoDb = document.getElementById('passCharCount');
+}
+
+document.addEventListener("DOMContentLoaded", init);
