366ac77d56ad300e3d7e9a735f6c37d75181711f
BatoBalint <balint.bato@gmail.com>
2021-11-04 21:28:09 +0100

    mukodik az input event

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-var nevDiv, nevDb, nevInput, jelszoDiv, jelszoDb, jelszoInput, jelszoMegintInput;
+var nevDiv;
+var nevDb;
+var nevInput;
+var jelszoDiv;
+var jelszoDb;
+var jelszoInput;
+var jelszoMegintInput;
+
+var regBtn;
 
 function init() {
   nevDiv = document.getElementById('nameCharCountDiv');
@@ -7,8 +15,22 @@ function init() {
   jelszoDb = document.getElementById('passCharCount');
 
   nevInput = document.getElementById('nameInput');
+  nevInput.addEventListener('input', nevInputChange);
+
   jelszoInput = document.getElementById('passInput');
-  jelszoMegintInput document.getElementById('passAgainInput');
+  jelszoMegintInput = document.getElementById('passAgainInput');
+
+  regBtn = document.getElementById('regBtn');
+  regBtn.addEventListener('click', regButtonCLick);
 }
 
 document.addEventListener("DOMContentLoaded", init);
+
+function nevInputChange() {
+  let length = nevInput.value.length;
+  console.log(length);
+}
+
+function regButtonCLick() {
+
+}
