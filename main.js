31c22990d8717f86c815036fa5296887f305bffd
BatoBalint <balint.bato@gmail.com>
2021-11-04 21:31:20 +0100

    mukodik a nev input teljesen

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var jelszoDb;
 var jelszoInput;
 var jelszoMegintInput;
 
+var error = false;
+
 var regBtn;
 
 function init() {
@@ -28,7 +30,12 @@ document.addEventListener("DOMContentLoaded", init);
 
 function nevInputChange() {
   let length = nevInput.value.length;
-  console.log(length);
+  nevDb.innerHTML = length;
+  if (length > 20) {
+    nevDiv.classList.add('text-danger');
+  } else if (length <= 20) {
+    nevDiv.classList.remove('text-danger');
+  }
 }
 
 function regButtonCLick() {
